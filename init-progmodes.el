53cc48126404391d99a6753e0a9257b2227d0c8a
unknown <share015@.wdw.disney.com>
2010-05-14 10:37:48 -0400

    Added Haskell Mode files and initialization into init-progmodes

--- init-progmodes.el
+++ init-progmodes.el
@@ -74,4 +74,15 @@
      (idle-highlight +1)
      (run-programming-hook))))
 
+
+;; --------------------------------------------------
+;;       Haskell Mode 
+;; --------------------------------------------------
+
+(load "haskell-site-file")
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'run-programming-hook)
+
 (provide 'init-progmodes)
