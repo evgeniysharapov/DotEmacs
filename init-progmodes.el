7918ae1f984f786db52f6f478a9b34b2be17e013
unknown <share015@.wdw.disney.com>
2010-07-08 09:57:07 -0400

    removed python code

--- init-progmodes.el
+++ init-progmodes.el
@@ -7,6 +7,18 @@
 ;; This variable contains functions that we need to run if we are programming 
 (defvar *programming-hook*)
 
+;; --------------------------------------------------
+;;  Autocomplete 
+;; --------------------------------------------------
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(defadvice ac-start (before advice-turn-on-auto-start activate)
+  (set (make-local-variable 'ac-auto-start) t))
+(defadvice ac-cleanup (after advice-turn-off-auto-start activate)
+  (set (make-local-variable 'ac-auto-start) nil))
+;; --------------------------------------------------
+;;  Other programming modes nifties 
+;; --------------------------------------------------
 (defun local-column-number-mode ()
   (make-local-variable 'column-number-mode)
   (column-number-mode t))
@@ -24,11 +36,12 @@
 (defun turn-on-whitespace ()
   (whitespace-mode t))
 
-(defun turn-on-paredit ()
-  (paredit-mode t))
-
 (defun turn-off-tool-bar ()
   (tool-bar-mode -1))
+
+(defun turn-on-linum-and-zenburn ()
+  (linum-mode)
+  (color-theme-zenburn))
 
 (defun add-watchwords ()
   (font-lock-add-keywords
@@ -41,6 +54,7 @@
 (add-hook '*programming-hook* 'turn-on-save-place-mode)
 (add-hook '*programming-hook* 'pretty-greek)
 (add-hook '*programming-hook* 'add-watchwords)
+(add-hook '*programming-hook* 'turn-on-linum-and-zenburn)
 
 (defun run-programming-hook ()
   "Enable things that are convenient across all coding buffers."
@@ -51,11 +65,11 @@
 ;; --------------------------------------------------
 (autoload 'paredit-mode "paredit" "Minor mode for pseudo-structurally editing Lisp code." t)
 
-(defun enable-paredit (keymap) 
+(defun turn-on-paredit (keymap) 
   (paredit-mode +1) 
   (define-key keymap (kbd "RET") 'paredit-newline))
 
-(add-hook 'emacs-lisp-mode-hook (lambda () (enable-paredit emacs-lisp-mode-map)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (turn-on-paredit emacs-lisp-mode-map)))
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'pretty-greek)
@@ -66,7 +80,6 @@
   ;  (font-lock-add-keywords
   ;   (intern (concat (symbol-name x) "-mode"))
   ;   '(("(\\|)" . 'esk-paren-face))))
-
   (add-hook
    (intern (concat (symbol-name x) "-mode-hook"))
    (lambda ()
@@ -74,137 +87,51 @@
      (idle-highlight +1)
      (run-programming-hook))))
 
+;; ;; these paredit keys are confusing
+;; (eval-after-load 'paredit
+;;   '(progn
+;;      ;; it's a habit to use C-Arrows to move over words 
+;;      (define-key paredit-mode-map  [(control right)] nil)
+     
+;;      (define-key paredit-mode-map  [(kbd "<M-down>")] nil)
+;;      (define-key paredit-mode-map "\M-r" nil)))
+
+;; ;; --------------------------------------------------
+;; ;;  Clojure
+;; ;; --------------------------------------------------
+;; (eval-after-load "slime"
+;;   '(progn
+;;      (require 'swank-clojure-extra)
+;;      (add-to-list 'slime-lisp-implementations
+;;                   `(clojure ,(swank-clojure-cmd)
+;;                             :init swank-clojure-init)
+;;                   t)
+;;      (add-hook 'slime-indentation-update-hooks 'swank-clojure-update-indentation)
+;;      (add-hook 'slime-repl-mode-hook 'swank-clojure-slime-repl-modify-syntax t)
+;;      (add-hook 'clojure-mode-hook 'swank-clojure-slime-mode-hook t)
+;;      (setq slime-highlight-compiler-notes nil)))
+
+;; ;; 
+
+
+
 
 ;; --------------------------------------------------
 ;;       Haskell Mode 
 ;; --------------------------------------------------
 
 (load "haskell-site-file")
-
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'run-programming-hook)
 
 ;; --------------------------------------------------
-;;  Autocomplete 
-;; --------------------------------------------------
-(require 'auto-complete)
-(global-auto-complete-mode t)
-
-;; --------------------------------------------------
 ;;       Python Mode 
 ;; --------------------------------------------------
-;;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-;;; (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-;;; (setf )
-;;; (require 'python-mode)
-(add-hook 'python-mode-hook
-      (lambda ()
-	(set-variable 'py-indent-offset 4)
-	;(set-variable 'py-smart-indentation nil)
-	(set-variable 'indent-tabs-mode nil)
-	(define-key py-mode-map (kbd "RET") 'newline-and-indent)
-	;(define-key py-mode-map [tab] 'yas/expand)
-	;(setq yas/after-exit-snippet-hook 'indent-according-to-mode)
-	(smart-operator-mode-on)
-	))
-;; pymacs
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
-;; Auto-completion integrates:
-;;   1) Rope
-;;   2) Yasnippet
-;;   all with AutoComplete.el
-(defun prefix-list-elements (list prefix)
-  (let (value)
-    (nreverse
-     (dolist (element list value)
-      (setq value (cons (format "%s%s" prefix element) value))))))
-(defvar ac-source-rope
-  '((candidates
-     . (lambda ()
-         (prefix-list-elements (rope-completions) ac-target))))
-  "Source for Rope")
-(defun ac-python-find ()
-  "Python `ac-find-function'."
-  (require 'thingatpt)
-  (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
-    (if (null symbol)
-        (if (string= "." (buffer-substring (- (point) 1) (point)))
-            (point)
-          nil)
-      symbol)))
-(defun ac-python-candidate ()
-  "Python `ac-candidates-function'"
-  (let (candidates)
-    (dolist (source ac-sources)
-      (if (symbolp source)
-          (setq source (symbol-value source)))
-      (let* ((ac-limit (or (cdr-safe (assq 'limit source)) ac-limit))
-             (requires (cdr-safe (assq 'requires source)))
-             cand)
-        (if (or (null requires)
-                (>= (length ac-target) requires))
-            (setq cand
-                  (delq nil
-                        (mapcar (lambda (candidate)
-                                  (propertize candidate 'source source))
-                                (funcall (cdr (assq 'candidates source)))))))
-        (if (and (> ac-limit 1)
-                 (> (length cand) ac-limit))
-            (setcdr (nthcdr (1- ac-limit) cand) nil))
-        (setq candidates (append candidates cand))))
-    (delete-dups candidates)))
-
-(add-hook 'python-mode-hook
-          (lambda ()
-                 (auto-complete-mode 1)
-                 (set (make-local-variable 'ac-sources)
-                      (append ac-sources '(ac-source-rope) '(ac-source-yasnippet)))
-                 (set (make-local-variable 'ac-find-function) 'ac-python-find)
-                 (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
-                 (set (make-local-variable 'ac-auto-start) nil)))
-
-;;Ryan's python specific tab completion
-(defun ryan-python-tab ()
-  ; Try the following:
-  ; 1) Do a yasnippet expansion
-  ; 2) Do a Rope code completion
-  ; 3) Do an indent
-  (interactive)
-  (if (eql (ac-start) 0)
-      (indent-for-tab-command)))
-
-(defadvice ac-start (before advice-turn-on-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) t))
-(defadvice ac-cleanup (after advice-turn-off-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) nil))
-
-(define-key py-mode-map "\t" 'ryan-python-tab)
-
-;; End Auto Completion
-
-;; Auto Syntax Error Hightlight
-;; (when (load "flymake" t)
-;;   (defun flymake-pyflakes-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;; 		       'flymake-create-temp-inplace))
-;; 	   (local-file (file-relative-name
-;; 			temp-file
-;; 			(file-name-directory buffer-file-name))))
-;;       (list "pyflakes" (list local-file))))
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;; 	       '("\\.py\\'" flymake-pyflakes-init)))
-;; (add-hook 'find-file-hook 'flymake-find-file-hook)
-
-
-
+;(load "python-mode-things")
+;(add-hook 'python-mode-hook 'sen/py-indentation)
+;(add-hook 'python-mode-hook 'sen/py-autocomplete)
+;(add-hook 'python-mode-hook 'sen/python-auto-fill-comments-only)
+;(add-hook 'python-mode-hook 'run-programming-hook)
 
 (provide 'init-progmodes)
