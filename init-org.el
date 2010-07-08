3c1d024092b55350a529172e10bf031f6c08b79f
unknown <share015@.wdw.disney.com>
2010-07-08 09:57:55 -0400

    added heading navigation using outline and follow-link is bound to RET

--- init-org.el
+++ init-org.el
@@ -4,7 +4,13 @@
 ;;  Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
 ;;
 
-(defun sen/org-mode-custmo-bindings ()
+(defun outline-down-heading ()
+  "Goes to the next heading with one level up, unless there's none, then 
+it goes to the next heading"
+  (interactive)
+  )
+
+(defun sen/org-mode-custom-bindings ()
   "customize org-mode keys"
   (local-set-key [(control up)] 'outline-previous-visible-heading)
   (local-set-key [(control down)]  'outline-next-visible-heading)
@@ -17,7 +23,13 @@
   (define-key yas/keymap [tab] 'yas/next-field-group)
   )
 
-(add-hook 'org-mode-hook 'sen/org-mode-custmo-bindings)
+(add-hook 'org-mode-hook 'sen/org-mode-custom-bindings)
 (add-hook 'org-mode-hook 'sen/org-mode-yasnippet-fix)
+(add-hook 'org-mode-hook 'yas/minor-mode)
+
+(add-hook 'org-mode-hook (lambda ()
+                           (setq org-return-follows-link t)  ; To follow links with RET
+                           ))
+
 
 (provide 'init-org)
