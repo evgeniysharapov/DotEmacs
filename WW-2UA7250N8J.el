e45edcddc990653737de9f6dcd1f269add1b4905
unknown <share015@.wdw.disney.com>
2010-07-12 13:02:53 -0400

    Added SSH configuration for editing files on remote servers

--- WW-2UA7250N8J.el
+++ WW-2UA7250N8J.el
@@ -11,3 +11,10 @@
 (global-set-key [(control shift f11)]
                 (make-interactive w32-send-sys-command #xf120 nil))
 (set-face-attribute 'default nil :family "Consolas" :height 100)
+
+;;
+;;  Setting SSH connection
+;;
+(require 'tramp)
+(setq tramp-default-method "plink")
+
