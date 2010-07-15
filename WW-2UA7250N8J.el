787f6e8dcf9a52527de4b2e2f648a03431448ace
unknown <share015@.wdw.disney.com>
2010-07-15 13:39:25 -0400

    added fontifying special variables in shell scripts

--- WW-2UA7250N8J.el
+++ WW-2UA7250N8J.el
@@ -17,4 +17,40 @@
 ;;
 (require 'tramp)
 (setq tramp-default-method "plink")
+;;
+;; Define new faces for shell
+;;
+;; (defface sh-special-vars
+;;   '((t (:weight bold :inherit font-lock-variable-name-face)))
+;;   "Shell special variables"
+;;   :group 'faces)
+
+;; (defface sh-var-content
+;;   '((t (:weight bold :inherit font-lock-variable-name-face)))
+;;   "Shell special variables"
+;;   :group 'faces)
+
+
+;; ;; XXX: redefining face defined in sh-script.el
+;; (defface sh-quoted-exec
+;;   '((((class color) (background light))
+;;      (:background "gray85")))
+;;   "Shell command substitution"
+;;   :group 'faces)
+
+;; (defun sen-sh-syntax ()
+;;   (font-lock-add-keywords 'sh-mode
+;;        '(("\\<\\(awk\\|alias\\|bg\\|bind\\|break\\|builtin\\|cd\\|command\\|continue\\|cut\\|declare\\|typeset\\|dirs\\|disown\\|echo\\|enable\\|eval\\|exec\\|exit\\|export\\|fc\\|fg\\|getopts\\|hash\\|help\\|history\\|jobs\\|kill\\|let\\|local\\|logout\\|popd\\|printf\\|pushd\\|pwd\\|read\\|readonly\\|return\\|sed\\|set\\|shift\\|shopt\\|suspend\\|test\\|times\\|trap\\|type\\|type\\|ulimit\\|umask\\|unalias\\|uniq\\|unset\\|wait\\)\\>" . 'font-lock-builtin-face)
+;;          ("${?[#@*$!?0-9]\\|PWD\\|UID\\OPTARG\\|OPTIND\\|PPID\\|HOSSTNAME}?" . 'sh-special-vars)
+;;          ("${?[a-zA-Z_][0-9a-zA-Z_]*}?" . sh-var-content)
+;;          )
+;;        ))
+(defface sh-special-variables-face
+  '((t (:weight bold :inherit font-lock-variable-name-face)))
+  "Shell special variables"
+  :group 'faces)
+(font-lock-add-keywords 'shell-script-mode
+                        '(
+                          ("${?\\([#@*$!?0-9]\\|PWD\\|UID\\OPTARG\\|OPTIND\\|PPID\\|HOSSTNAME\\)}?" . 'sh-special-variables-face)
+                          ))
 
