823efff77316141cb1c1c99950b851b8c9fd8295
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2010-07-17 12:15:11 -0400

    Added custom.el file

---
+++ custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(color-theme-is-cumulative nil)
+ '(column-number-mode t)
+ '(custom-buffer-done-kill t)
+ '(inferior-lisp-program "clj")
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
+ '(org-empty-line-terminates-plain-lists t)
+ '(recentf-max-saved-items 200)
+ '(scalable-fonts-allowed t)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 10000)
+ '(scroll-margin 5)
+ '(scroll-preserve-screen-position t)
+ '(scroll-step 1)
+ '(show-paren-mode t)
+ '(show-paren-style (quote mixed))
+ '(use-dialog-box nil)
+ '(visible-bell t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "gray50" :slant italic))))
+ '(font-lock-doc-face ((t (:inherit font-lock-string-face :background "gray90" :slant oblique))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "ForestGreen"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background light)) (:foreground "Purple" :weight semi-bold))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "RoyalBlue4"))))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background light)) (:foreground "VioletRed4"))))
+ '(sh-quoted-exec ((((class color) (background light)) (:background "gray85")))))
