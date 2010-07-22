c0b4df508b82ff9b05167f5e2e35c36e66548a11
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2010-07-22 16:14:53 -0400

    Added Muse initialization

---
+++ init-muse.el
@@ -0,0 +1,11 @@
+;;  
+;;  Emacs Configuration
+;;  Setup for a Muse Mode 
+;;  Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
+;;
+
+(require 'muse-mode)
+(require 'muse-publish)
+(require 'muse-html)
+
+(provide 'init-muse)
