013d51a6d3af73032992b2deb44ea86a2327d5a7
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2010-08-17 18:09:54 -0400

    Muse mode is now working

--- init-muse.el
+++ init-muse.el
@@ -4,8 +4,15 @@
 ;;  Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
 ;;
 
-;; (require 'muse-mode)
-;; (require 'muse-publish)
-;; (require 'muse-html)
+(require 'muse-mode)
+(require 'muse-publish)
+(require 'muse-html)
+
+;; do the setup properly
+;; create a lo
+(add-hook 'muse-mode-hook 'footnote-mode)
+(add-hook 'muse-mode-hood
+          (lambda ()
+            (turn-on-auto-fill)))
 
 (provide 'init-muse)
