23b34a97be736238f9911b31724b899939e87f48
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2010-09-02 09:57:51 -0400

    Added Dired launching to IDO

--- init-general.el
+++ init-general.el
@@ -93,7 +93,8 @@
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-use-filename-at-point 'guess
-      ido-max-prospects 10)
+      ido-max-prospects 10
+      ido-show-dot-for-dired t)
 
 ;;;; add Smex 
 (if (fboundp 'smex-initialize) (progn 
