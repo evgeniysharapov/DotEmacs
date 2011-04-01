7d508b293b7819b811587a936d666379383a5d81
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-04-01 15:01:10 -0400

    Removed content of site-lisp/ELPA

--- site-lisp/elpa/css-mode-1.0/css-mode-autoloads.el
+++
@@ -1,12 +0,0 @@
-;;; css-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; css-mode-autoloads.el ends here
--- site-lisp/elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19432 28157
-;;;;;;  793000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
