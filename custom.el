b4ec3c5ce360c3c2e0b42bde15e3a81f585d7772
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-04-19 17:58:04 -0400

    Added more Tango and Blackboard color themes and changed color-theme to be cumulative.

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(color-theme-is-cumulative nil)
+ '(color-theme-is-cumulative t)
  '(column-number-mode t)
  '(compilation-scroll-output (quote first-error))
  '(custom-buffer-done-kill t)
