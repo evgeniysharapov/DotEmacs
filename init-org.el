e6cdba53eeaabcea6aaf299ed21cb0108286648f
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-04-19 12:19:30 -0400

    Wrapped ysnipped mode for org-mode initialization into (if fboundp)

--- init-org.el
+++ init-org.el
@@ -33,7 +33,8 @@ it goes to the next heading"
            org-completion-use-ido t)
      (sen/org-mode-custom-bindings)
      (sen/org-mode-yasnippet-fix)
-     (yas/minor-mode-on)
+     (if (fboundp 'yas/minor-mode-on)
+         (yas/minor-mode-on))
      (hl-line-mode +1)
      (turn-on-auto-fill)))
 
