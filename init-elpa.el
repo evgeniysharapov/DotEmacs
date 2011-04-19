0de6cf1c7a17e6f46f8f7f9df21c6b4614e92a44
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-04-19 11:14:13 -0400

    Cleaned up ELPA initialization

--- init-elpa.el
+++ init-elpa.el
@@ -5,46 +5,12 @@
 ;;
 (require 'package)
 (setq package-user-dir (concat site-lisp "/elpa"))
-;(package-initialize)
 
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ))
 
-(defvar default-elpa-packages (list 'paredit
-                                    'smart-tab
-                                    'yasnippet-bundle
-                                    'smex
-                                    'org)
-  "Libraries that should be installed by default.")
-
-
-(defun elpa-install ()
-  "Install all packages that aren't installed."
-  (interactive)
-  (dolist (pkg default-elpa-packages)
-    (unless (or (member pkg package-activated-list)
-                (functionp pkg))
-      (message "Installing %s" (symbol-name pkg))
-      (package-install pkg))))
-
-(defun online? ()
-  "See if we're online. Windows does not have the network-interface-list function, so we
-just have to assume it's online."
-  ;; TODO how could this work on Windows?
-  (if (and (functionp 'network-interface-list)
-           (network-interface-list))
-      (some (lambda (iface) (unless (equal "lo" (car iface))
-                         (member 'up (first (last (network-interface-info
-                                                   (car iface)))))))
-            (network-interface-list))
-    t))
-
-;; On your first run, this should pull in all the base packages.
-(when (online?)
-  (unless package-archive-contents (package-refresh-contents))
-  (elpa-install))
-
+(package-initialize)
 
 (provide 'init-elpa)
