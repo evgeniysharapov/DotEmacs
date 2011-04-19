d38252154883b56c203389f25d424391b2743eb9
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-04-19 14:29:47 -0400

    Removed trailing whitespaces.

--- init-general.el
+++ init-general.el
@@ -1,19 +1,19 @@
-;;  
+;;
 ;;  New Emacs Configuration
 ;;  General Settings ( Look and Feel, etc.)
 ;;  Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
 ;;
 
 ;; --------------------------------------------------
-;;           GUI/Look and Feel 
+;;           GUI/Look and Feel
 ;; --------------------------------------------------
 
-;; Turn off bells and whistles 
+;; Turn off bells and whistles
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; we want to know what is the file name 
+;; we want to know what is the file name
 (when window-system
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))
   (mouse-wheel-mode t)
@@ -23,46 +23,46 @@
 ;; Initialize Color-Theme
 ;;
 (if (fboundp 'color-theme-initialize)
-	(progn 
+	(progn
 	 (setq color-theme-is-global nil)
 	 (color-theme-initialize)))
 
 (display-time)
 ;; --------------------------------------------------
-;;        Files Settings and Operations 
+;;        Files Settings and Operations
 ;; --------------------------------------------------
 
 ;; Save a list of recent files visited.
 (recentf-mode 1)
 
-;;  File associations 
+;;  File associations
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 
-;; Backups and saves 
+;; Backups and saves
 (setq save-place-file (concat *dotfiles-dir* "places"))
 (setq backup-directory-alist `(("." . ,(expand-file-name
                                         (concat *dotfiles-dir* "backups")))))
 
 ;; --------------------------------------------------
-;;                    Byte Compilation 
+;;                    Byte Compilation
 ;; --------------------------------------------------
 (setq byte-compile-verbose nil)
 (setq byte-compile-warnings (quote (unresolved obsolete suspicious)))
 (setq byte-cache-directory nil)
 (setq bcc-enabled-on-save t)
-(setq bcc-blacklist '("/\\.recentf$" "/history$" "/\\.ecb-user-layouts\\.el$" "/\\.session$" "/\\.emacs-project$" 
-			"/\\.emacs\\.desktop$" "/custom\\.el$" "/init\\.el$" "/\\.ido\\.last$" "/\\.ecb-tip-of-day\\.el$" 
+(setq bcc-blacklist '("/\\.recentf$" "/history$" "/\\.ecb-user-layouts\\.el$" "/\\.session$" "/\\.emacs-project$"
+			"/\\.emacs\\.desktop$" "/custom\\.el$" "/init\\.el$" "/\\.ido\\.last$" "/\\.ecb-tip-of-day\\.el$"
 			"/\\.viper$" "/\\.recentf$" "cal-loaddefs\\.el" "esh-groups\\.el"  "tramp-loaddefs\\.el" "finder-inf\\.el"))
 (require 'byte-code-cache nil t)
 
 ;; --------------------------------------------------
-;;                    Buffers 
+;;                    Buffers
 ;; --------------------------------------------------
 
-;; Encoding and text related stuff 
+;; Encoding and text related stuff
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
@@ -104,13 +104,13 @@ Example:
   (my/display-prev-next-buffers))
 
 ;; --------------------------------------------------
-;;   Windows and stuff 
+;;   Windows and stuff
 ;; --------------------------------------------------
 (when (fboundp 'winner-mode)
       (winner-mode 1))
 
 ;; --------------------------------------------------
-;;    Miscellaneous 
+;;    Miscellaneous
 ;; --------------------------------------------------
 (setq redisplay-dont-pause t)
 
@@ -118,13 +118,13 @@ Example:
 
 (random t)
 
-;;;; Setup IDO mode 
+;;;; Setup IDO mode
 (ido-mode t)
 (ido-everywhere t)
 
-;;;; add Smex 
+;;;; add Smex
 (when (fboundp 'smex-initialize)
-    (progn 
+    (progn
       (smex-initialize)
       (global-set-key (kbd "M-x") 'smex)
       (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
