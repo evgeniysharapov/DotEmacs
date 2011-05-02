96f8c0f972dda2a1c0d3dbe6768f9be62d1784f6
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-05-02 15:41:25 -0400

    Fixed conflict of Windmove and Org-mode shift arrows keys. Now they work in Org-mode.

--- init-org.el
+++ init-org.el
@@ -40,6 +40,13 @@ it goes to the next heading"
 
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 
+
+;;; Fix conflict of Windmove and Org mode
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 ;;
 ;;  Setup iimage working with Org-mode
 ;; 
