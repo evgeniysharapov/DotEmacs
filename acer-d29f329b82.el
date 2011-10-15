919e485d720f4f8b1bedf6e7ca82e1dfa47886f7
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-10-16 00:52:11 +0600

    Added keyboard shortcuts to fullscreen the Emacs window. Added Consolas as default typeface.

--- acer-d29f329b82.el
+++ acer-d29f329b82.el
@@ -2,3 +2,12 @@
 ;;  Evgeniy Sharapov 
 ;;  this is a sppecifics config file for mom's laptop in Perm, Russia
 ;;
+
+; maximize Emacs frame
+(global-set-key [(control f11)]
+                (make-interactive w32-send-sys-command #xf030 nil))
+; restore original size of the Emacs frame
+(global-set-key [(control shift f11)]
+                (make-interactive w32-send-sys-command #xf120 nil))
+; set font for the Windows
+(set-face-attribute 'default nil :family "Consolas" :height 100)
