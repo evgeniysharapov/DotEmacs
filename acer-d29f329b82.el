a5e9a0994baeb43b6309bfed3d7bbbe9ec5a77d9
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-10-17 02:51:57 +0600

    Removed repeated section about fullscreen mode and consolas as a default font

--- acer-d29f329b82.el
+++ acer-d29f329b82.el
@@ -3,11 +3,3 @@
 ;;  this is a sppecifics config file for mom's laptop in Perm, Russia
 ;;
 
-; maximize Emacs frame
-(global-set-key [(control f11)]
-                (make-interactive w32-send-sys-command #xf030 nil))
-; restore original size of the Emacs frame
-(global-set-key [(control shift f11)]
-                (make-interactive w32-send-sys-command #xf120 nil))
-; set font for the Windows
-(set-face-attribute 'default nil :family "Consolas" :height 100)
