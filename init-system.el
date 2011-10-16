1e983d49a2221943635bc7626f052e897a141eed
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-10-17 02:55:50 +0600

    Fixed init-system

--- init-system.el
+++ init-system.el
@@ -17,5 +17,8 @@
   (global-set-key [(control shift f11)]
                   (make-interactive w32-send-sys-command #xf120 nil))
                                         ; set font for the Windows
-  (set-face-attribute 'default nil :family "Consolas" :height 100)
-  )
+  (set-face-attribute 'default nil :family "Consolas" :height 100))
+
+
+
+(provide 'init-system)
