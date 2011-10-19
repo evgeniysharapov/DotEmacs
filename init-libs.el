184b77455464f5425e6158d14595998b87d1c4c9
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-10-19 23:21:02 +0600

    commented out ELPA package storage.

--- init-libs.el
+++ init-libs.el
@@ -6,10 +6,11 @@
 (require 'package)
 (setq package-user-dir (concat site-lisp "/elpa"))
 
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
+(setq package-archives '(;("ELPA" . "http://tromey.com/elpa/") 
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ))
+;; ELPA packages that should be installed by default
 
 (package-initialize)
 
