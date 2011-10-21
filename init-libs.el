dd7ff2d7a1810b76c2b822575f62faebf6c05a16
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-10-21 20:20:38 +0600

    Changed to use built-in package.el that comes with Emacs 24. Added require-package that loads and installs the package. Few packages are added as default, i.e. we don't work on emacs without these packages.

--- init-libs.el
+++ init-libs.el
@@ -5,17 +5,44 @@
 ;;  Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
 ;;
 
+(defun require-package (package &optional min-version)
+  "Installs package of desired version using ELPA or el-get"
+  ;; TODO: make it work with minimum and maximum version
+  ;; use version-list-= and version-list-> from subr.el
+  (unless (package-installed-p package min-version)
+    (package-install package)))
 
-(require 'package)
-;; all ELPA packages are located here
-(setq package-user-dir (concat *site-lisp* "/elpa"))
 
-(setq package-archives '(;("ELPA" . "http://tromey.com/elpa/") 
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ))
-;; ELPA packages that should be installed by default
+(when (require 'package nil 'noerror)
+  
+  ;; all ELPA packages are located here
+  (setq package-user-dir (concat *site-lisp* "/elpa"))
+  ;; ELPA extensions repos 
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
-(package-initialize)
+  ;; ELPA packages that should be installed by default
+  (package-initialize)
+
+  (unless package-archive-contents
+    (package-refresh-contents))
+
+  ;; ELPA packages that I would like to have installed
+  (require-package 'magit)
+
+  (require-package 'idle-highlight-mode)
+  (require-package 'highlight-symbol)
+  (require-package 'rainbow-mode)
+  (require-package 'rainbow-delimiters)
+  
+  (require-package 'paredit)
+  (require-package 'slime)
+  
+  (require-package 'ido-yes-or-no)
+  (require-package 'smex)
+  (require-package 'yasnippet-bundle)
+  (require-package 'undo-tree)
+  
+  )
 
 (provide 'init-libs)
