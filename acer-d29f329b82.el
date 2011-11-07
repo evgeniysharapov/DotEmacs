1f10f47a3add6b0cbc16f0f633319e16ca64a1f0
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-11-07 23:40:43 +0500

    Configured git executable for magit on Windows for Acer laptop and Inna's PC.

--- acer-d29f329b82.el
+++ acer-d29f329b82.el
@@ -1,3 +1,4 @@
+
 ;;
 ;;  Evgeniy Sharapov 
 ;;  this is a sppecifics config file for mom's laptop in Perm, Russia
@@ -5,4 +6,7 @@
 
 ;;; set exec-path for mom's laptop
 (add-to-list 'exec-path "C:/Program Files/Aspell/bin")
+
+;;; set up git executable for magit on Mom's laptop 
+(setq magit-git-executable "C:/Program Files/Git/bin/git.exe")
 
