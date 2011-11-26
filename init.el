74e35445fb7f9356b6547bd18866602cbd21f27f
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2011-11-26 23:18:10 +0500

    Made adding directories to the load-path simpler

--- init.el
+++ init.el
@@ -15,30 +15,10 @@
 ;; Set up local path list (from where we will regenerate autoloads 
 (setq local-load-path (list *site-lisp*))
 
-(defun add-paths (this-directory path-list &optional with-subdirs recursive) 
-  "Add THIS-DIRECTORY at the beginning of the load-path, if it exists. Add all its subdirectories not starting with a '.' if the optional argument WITH-SUBDIRS is not nil. Do it recursively if the third argument is not nil."
-  (when this-directory 
-    (when (file-directory-p this-directory) 
-      (let* ((this-directory (expand-file-name this-directory)) 
-             (files (directory-files this-directory t "^[^\\.]")))
-        ;; completely canonicalize the directory name (*may not* begin with `~')
-        (while (not (string= this-directory (expand-file-name this-directory))) 
-          (setq this-directory (expand-file-name this-directory))) 
-        
-	(message "Adding `%s' to load-path..." this-directory) 
-        (add-to-list path-list this-directory) 
-        (if with-subdirs 
-            (progn 
-              (while files 
-                (setq dir-or-file (car files)) 
-                (when (file-directory-p dir-or-file) 
-                  (if recursive 
-		     (add-paths dir-or-file path-list 'with-subdirs)
-                     (add-paths dir-or-file path-list))) 
-                (setq files (cdr files)))))))))
-
 ;; add recursively all subdirectories of *site-lisp* 
-(add-paths *site-lisp* 'local-load-path 'with-subdirs 'recursive)
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+    (let ((default-directory *site-lisp*))
+      (normal-top-level-add-subdirs-to-load-path)))
 
 ;; add it to the load path 
 (dolist (path local-load-path) (add-to-list 'load-path path))
