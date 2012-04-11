7190740d7e3beeb93820adb9b60c310e675ffdec
unknown <sharapove@sharapove.prognoz.ru>
2012-04-11 11:13:38 +0600

    Changed rainbow-delimiters scheme.

--- custom.el
+++ custom.el
@@ -74,12 +74,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "light slate gray"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "sky blue"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "lime green"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "khaki"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "salmon"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "pink"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "violet"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "medium purple"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dim gray"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "SkyBlue3"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "aquamarine4"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "khaki4"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "IndianRed2"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "burlywood4"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "chocolate2"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "orange3"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "HotPink3"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "DarkRed"))))
  '(trailing-whitespace ((t (:background "IndianRed1")))))
