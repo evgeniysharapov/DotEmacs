acb36a60e1d0374db2b6c7694339e4f350b21cfb
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-10-21 22:16:37 +0600

    adjusted PATH variable and exec-path to sync with what is in the terminal for emacs that started from the GUI on Mac OS X.

--- senmb.local.el
+++ senmb.local.el
@@ -11,3 +11,14 @@
 ;; TODO: justify for the face font size 
 (if (window-system)
     (set-frame-size (selected-frame) 165 42))
+
+;;; adjust PATH in emacs to what it is in the terminal
+(setenv "PATH"
+        (with-temp-buffer
+          (call-process (getenv "SHELL") nil (current-buffer) nil "--login" "-i" "-c" "echo \"$PATH\"")
+          (car (split-string (buffer-string) "\n" t))))
+(setq exec-path (split-string (getenv "PATH") path-separator t))
+
+
+
+
