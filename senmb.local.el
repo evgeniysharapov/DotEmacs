6c9a29151f0c543ed3c0c2ac9105db80109c6ed1
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2013-05-03 19:15:35 -0400

    setup spellchecker specific to Mac book

--- senmb.local.el
+++ senmb.local.el
@@ -14,6 +14,12 @@
       (set-frame-size (selected-frame) 125 40)
       (set-face-attribute 'default nil :family "Menlo" :height 120)))
 
+;;; Spellchecker setup
+(eval-after-load "ispell"
+  '(progn
+    (when (executable-find "hunspell")
+      (setq ispell-program-name "hunspell"
+            ispell-extra-args '("-d" "/opt/local/share/hunspell/en_US" "-i" "utf-8")))))
 ;;; adjust PATH in emacs to what it is in the terminal
 (setenv "PATH"
         (with-temp-buffer
