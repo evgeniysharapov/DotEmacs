6d61ffb38026c842a1c7fc3dae9c16ffd0e4c2b8
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2013-10-07 22:31:03 -0400

    added some face customizations for mac.

--- senmb.local.el
+++ senmb.local.el
@@ -14,6 +14,15 @@
   ;; on Mac emacs typefonts look better in dark
   (load-theme 'base16-default t))
 
+(defun ffy-adjust-faces ()
+  "This one changes faces that come with tomorrow theme and look better on the Macbook DELL screen. Probably should go into a theme."
+  (dolist (face-spec '((region (t (:background "#404040")))
+                       (highlight (t (:background "slate gray")))))
+     (face-spec-set (car face-spec)
+                    (cdr face-spec))))
+
+(add-hook 'after-init-hook 'ffy-adjust-faces)
+
 ;;; Spellchecker setup
 (eval-after-load "ispell"
   '(progn
@@ -26,7 +35,3 @@
           (call-process (getenv "SHELL") nil (current-buffer) nil "--login" "-i" "-c" "echo \"$PATH\"")
           (car (split-string (buffer-string) "\n" t))))
 (setq exec-path (split-string (getenv "PATH") path-separator t))
-
-
-
-
