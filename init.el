eddb7909ac6be8a61415bbbb059c642dd7207c03
Evgeniy Sharapov <evgeniy.sharapov@stoneriver.com>
2013-11-01 17:34:59 -0400

    Changed JavaScript setup.

--- init.el
+++ init.el
@@ -1253,7 +1253,7 @@ Implementation shamelessly stolen from: https://github.com/jwiegley/dot-emacs/bl
 ;;; ----------------------------------------------------------------------
 ;;; Good link about setting up Javascript:
 ;;; http://blog.deadpansincerity.com/2011/05/setting-up-emacs-as-a-javascript-editing-environment-for-fun-and-profit/
-
+(require-package 'js2-mode)
 (require-package 'flymake-jslint)
 (require-package 'flymake-jshint)
 (require-package 'js-comint)
@@ -1267,16 +1267,27 @@ Implementation shamelessly stolen from: https://github.com/jwiegley/dot-emacs/bl
 
 (add-hook 'js-mode-hook 'ffy-js-mode-customizations)
 ;; Use node as our repl
-(setq inferior-js-program-command (executable-find "node"))
-(setq inferior-js-mode-hook
-      (lambda ()
-        ;; We like nice colors
-        (ansi-color-for-comint-mode-on)
-        ;; Deal with some prompt nonsense
-        (add-to-list 'comint-preoutput-filter-functions
-                     (lambda (output)
-                       (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
-                     (replace-regexp-in-string ".*1G.*3G" "&gt;" output))))
+;; (setq inferior-js-program-command (executable-find "node"))
+
+;; (setq inferior-js-mode-hook
+;;       (lambda ()
+;;         ;; We like nice colors
+;;         (ansi-color-for-comint-mode-on)
+;;         ;; Deal with some prompt nonsense
+;;         (add-to-list
+;;          'comint-preoutput-filter-functions
+;;          (lambda (output)
+;;            (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
+
+;; (setq inferior-js-mode-hook
+;;       (lambda ()
+;;         ;; We like nice colors
+;;         (ansi-color-for-comint-mode-on)
+;;         ;; Deal with some prompt nonsense
+;;         (add-to-list 'comint-preoutput-filter-functions
+;;                      (lambda (output)
+;;                        (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
+;;                      (replace-regexp-in-string ".*1G.*3G" "&gt;" output))))
 
 ;;;_. Scala setup
 (require-package 'scala-mode)
