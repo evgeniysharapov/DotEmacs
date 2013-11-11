5469a2aa25dd4d0b59afd09004137aeed83f22bf
Evgeniy Sharapov <evgeniy.sharapov@stoneriver.com>
2013-11-11 09:38:34 -0500

    removed some work with ELPA.

--- init.el
+++ init.el
@@ -64,26 +64,13 @@
 ;;;_. ELPA packages
 
 ;;;_ , Generating autoloads file from the installed packages
-;;;_  . ffy-find-package-autoloads-file
-(defun ffy-find-package-autoloads-file (package)
-  "Finds autoload file for the ELPA package and returns the path to the file, with a  check for file existence if performed, or nil.
-One of the examples of using the 
-
- (ffy-find-package-autoloads-file 'haskell-mode)"
-  ;; get version of the package version list to string 
-  (let* ((pkg-str (if (symbolp package) (symbol-name package) package))
-         (pkg-ver-str (mapconcat 'number-to-string 
-                                 (elt (cdr  (assq package package-alist)) 0) "."))
-         (pkg-al-file (concat (package--dir pkg-str pkg-ver-str) "/" pkg-str "-autoloads.el")))
-    ;; check file existence 
-    (when (file-exists-p pkg-al-file)
-      pkg-al-file)))
 ;;;_  . close-autoloads advice
 (defadvice package-generate-autoloads (after close-autoloads (name pkg-dir) activate)
   "Stop package.el from leaving open autoload files lying around."
   (let ((path (expand-file-name (concat name "-autoloads.el") pkg-dir)))
     (with-current-buffer (find-file-existing path)
       (kill-buffer nil))))
+
 ;;;_  . extract-autoloads
 (defun extract-autoloads ()
   "Extract autoloads recursively from *SITE-LISP* and puts it into *AUTOLOAD-FILE*"
@@ -96,26 +83,9 @@ One of the examples of using the
                          if (file-directory-p d)
                          collect d)))
     (apply 'update-directory-autoloads dir-list)))
+
 ;;;_  . Extract autoloads on killing Emacs
 (add-hook 'kill-emacs-hook 'extract-autoloads)
-
-;;;_ , Add support to package.el for pre-filtering available packages
-(defvar package-filter-function nil
-  "Optional predicate function used to internally filter packages used by package.el.
-
-The function is called with the arguments PACKAGE VERSION ARCHIVE, where
-PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
-ARCHIVE is the string name of the package archive.")
-
-(defadvice package--add-to-archive-contents
-  (around filter-packages (package archive) activate)
-  "Add filtering of available packages using `package-filter-function', if non-nil."
-  (when (or (null package-filter-function)
-            (funcall package-filter-function
-                     (car package)
-                     (package-desc-vers (cdr package))
-                     archive))
-    ad-do-it))
 
 ;;;_ , Add requiring package from ELPA (install if not installed)
 (defun require-package (package &optional min-version)
@@ -132,23 +102,10 @@ ARCHIVE is the string name of the package archive.")
 (when (require 'package nil 'noerror)
   ;; all ELPA packages are located here
   (setq package-user-dir (concat *dotfiles-dir* "elpa"))
-;;;_  . Don't take Melpa versions of certain packages
-  (setq package-filter-function
-	(lambda (package version archive)
-	  (or (not (string-equal archive "melpa"))
-	      (not (memq package
-			 '(
-			   ruby-compilation
-			   slime
-			   color-theme-sanityinc-solarized
-			   color-theme-sanityinc-tomorrow
-			   elisp-slime-nav
-			   findr))))))
 ;;;_  . Sources for the ELPA repositories 
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
   (package-initialize)
   (unless package-archive-contents
