4fa74896c63cf9b181bdc14f987adfc8fa50c679
Evgeniy Sharapov <evgeniy.sharapov@stoneriver.com>
2014-01-13 16:14:05 -0500

    JavaScript files are hooked onto js-mode.

--- init.el
+++ init.el
@@ -1356,15 +1356,17 @@ Implementation shamelessly stolen from: https://github.com/jwiegley/dot-emacs/bl
              ;; Activate the folding mode
              (hs-minor-mode t))
 
-           (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-           (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode)))
+           (add-to-list 'interpreter-mode-alist '("node" . js-mode))
+           (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+           (add-to-list 'auto-mode-alist '("\\.json$" . js-mode)))
 
   :config (progn
             (add-hook 'js-mode-hook 'ffy-js-mode-customizations)
             (add-hook 'js-mode-hook 'js2-minor-mode)
             (add-hook 'js-mode-hook 'turn-on-electric-mode)
+            (add-hook 'js-mode-hook 'turn-on-flymake)
             ;;; Add Auto-Complete to JavaScript modes.
-            (dolist (mode '(espresso-mode js2-mode js3-mode))
+            (dolist (mode '(espresso-mode js2-mode))
               (add-to-list 'ac-modes mode))))
 
 ;; (setq inferior-js-mode-hook
