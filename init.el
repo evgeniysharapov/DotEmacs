2ba498ad22aff1bb0bc44f45df8cb8b28e685c82
Evgeniy Sharapov <evgeniy.sharapov@stoneriver.com>
2014-06-15 19:22:57 -0400

    Fix: generated tangle encoding due to the UTF characters in the config file.

--- init.el
+++ init.el
@@ -15,12 +15,14 @@
   (toggle-debug-on-error)
   (require 'org)
   (require 'ob-tangle)
-          
+  ;; To make ob-tangle happy even if I have crazy UTF characters
+  ;; in my config org-mode file
+  ;; for more see https://lists.gnu.org/archive/html/emacs-orgmode/2010-11/msg00951.html
+  (add-hook 'org-babel-pre-tangle-hook
+          (lambda ()
+	    (setq default-buffer-file-coding-system 'utf-8)))
   ;; load up Emacs configuraion from the Org file next to init.el
   ;; filename is evaled while macros is expanded, otherwise buffer-file-name would be nil
   (org-babel-load-file (expand-file-name "configuration.org" ,(file-name-directory (or load-file-name (buffer-file-name)))))
   ;; if we loaded it up, use normal error throwing
   (toggle-debug-on-error)))
-
-
-                       
