a80d8666d2fc43c91433e91d73a60f07044cbaab
Evgeniy Sharapov <esharapov@infusion.com>
2014-11-15 11:01:04 -0500

    Fixed setting 'exec-path based on shell path not working in Emacs 24.4

--- systems/senmb.local.el
+++ systems/senmb.local.el
@@ -31,9 +31,12 @@
     (when (executable-find "hunspell")
       (setq ispell-program-name "hunspell"
             ispell-extra-args '("-d" "/opt/local/share/hunspell/en_US" "-i" "utf-8")))))
-;;; adjust PATH in emacs to what it is in the terminal
-(setenv "PATH"
-        (with-temp-buffer
-          (call-process (getenv "SHELL") nil (current-buffer) nil "--login" "-i" "-c" "echo \"$PATH\"")
-          (car (split-string (buffer-string) "\n" t))))
-(setq exec-path (split-string (getenv "PATH") path-separator t))
+
+;;; adjust exec-path in emacs to what it is in the terminal
+(let ((shell-path (split-string (with-temp-buffer
+                                        (call-process (getenv "SHELL") nil (current-buffer) nil "--login" "-i"  "-c" "echo \"$PATH\"")
+                                        (car (split-string (buffer-string) "\n" t))) path-separator t)))
+  (princ  shell-path)
+  (setq exec-path (append exec-path shell-path)))
+
+
