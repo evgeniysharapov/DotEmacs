7abda8d7bfe10c41b1fc123ef60c44698ad54795
Evgeniy Sharapov <esharapov@infusion.com>
2014-12-09 14:52:53 -0500

    javascript back to JS2

--- custom.el
+++ custom.el
@@ -78,6 +78,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flymake-errline ((t (:inherit error :underline (:color foreground-color :style wave)))))
- '(flymake-warnline ((t (:inherit warning :underline (:color foreground-color :style wave)))))
+ '(js2-warning ((t (:underline (:color "orange" :style wave)))))
  '(magit-item-highlight ((t (:inherit hl-line)))))
