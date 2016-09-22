8b43828b91e24365fc561e7a67546ecc9c394fce
Evgeniy Sharapov <esharapov@infusion.com>
2016-09-22 12:57:29 -0400

    added paredit, slime-navigation and eldoc to elisp modes.

--- init.el
+++ init.el
@@ -236,6 +236,18 @@
 		 ;; mode anyway
 		 (bind-key "M-k" #'paredit-splice-sexp paredit-mode-map)))
 
+(use-package elisp-slime-nav
+  :ensure t
+  :commands elisp-slime-nav-mode
+  :diminish elisp-slime-nav-mode)
+
+(setq *lisp-mode-hooks* '(emacs-lisp-mode-hook ielm-mode-hook lisp-mode-hook lisp-interaction-mode-hook))
+
+(dolist (mode-hook *lisp-mode-hooks*)
+  (add-hook mode-hook #'paredit-mode)
+  (add-hook mode-hook #'elisp-slime-nav-mode)
+  (add-hook mode-hook #'eldoc-mode))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;			   Load custom-vars File
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
