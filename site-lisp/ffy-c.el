e943151da7c11718cd1e4b5ab761128922574382
Evgeniy Sharapov <esharapov@infusion.com>
2016-09-26 17:47:10 -0400

    added c/c++ configuration for kattis

---
+++ site-lisp/ffy-c.el
@@ -0,0 +1,58 @@
+;;; C/C++ Configuration
+
+(use-package cc-mode
+  :mode (("\\.h\\(h?\\|xx\\|pp\\)\\'" . c++-mode)
+         ("\\.m\\'"                   . c-mode)
+         ("\\.mm\\'"                  . c++-mode)))
+
+;;; C/C++ Headers Locations. This is system specific
+(setq ffy-c-headers-locations '("C:/PF86/LLVM/lib/clang/3.6.2/include"
+                                "C:/PF86/LLVM/i686-w64-mingw32/include"
+                                "C:/PF86/LLVM/i686-w64-mingw32/include/c++"
+                                "C:/PF86/LLVM/i686-w64-mingw32/include/c++/i686-w64-mingw32"
+                                "C:/PF86/LLVM/include/"))
+
+(use-package company-c-headers
+  :defer t
+  :ensure t
+  :config
+  (dolist (dir (if (boundp 'ffy-c-headers-locations)
+		   ffy-c-headers-locations
+		 ()))
+    (add-to-list 'company-c-headers-path-system dir)))
+
+(use-package c-eldoc
+  :defer t
+  :ensure t
+  :config (setq c-eldoc-includes
+                (mapconcat #'identity
+                           ;; on Windows `pkg-config` .... leads to an
+                           ;; error
+                           (cons ;c-eldoc-includes
+                                 "-I. -I.."
+                                 (mapcar (apply-partially #'concat "-I")
+                                         ffy-c-headers-locations))
+                           " ")
+                c-eldoc-cpp-command "cpp"))
+
+(defun ffy-c-mode-hook ()
+  "This is settings for the C/C++ mode"
+  (when (memq major-mode '(c-mode c++-mode))
+    (electric-pair-mode +1)
+    (electric-indent-local-mode +1)
+    (c-toggle-hungry-state +1)
+    (c-set-style "gnu")
+    (setq c-basic-offset 4)
+    (set (make-local-variable 'compile-command)
+         (let ((f (file-name-nondirectory (buffer-file-name))))
+           (case major-mode
+             ('c-mode (format "gcc -g -O2 -std=gnu99 -static -lm %s" f))
+             ('c++-mode (format "g++ -g -O2 -static -std=gnu++11 %s" f))
+             (t compile-command))))
+    (ffy-add-company-backends 'company-c-headers 'company-semantic 'company-clang 'company-xcode)))
+
+;;; due to a bug http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16759 we
+;;; add it to a c-mode-common-hook
+(add-hook 'c-mode-common-hook #'ffy-c-mode-hook)
+
+(provide 'ffy-c)
