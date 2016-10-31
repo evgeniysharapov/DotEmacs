006ba36101e9548c21426c2fd49e4803fc7b626d
Evgeniy Sharapov <esharapov@infusion.com>
2016-10-31 16:46:10 -0400

    corrected python auto-completion in interpreter

--- site-lisp/ffy-python.el
+++ site-lisp/ffy-python.el
@@ -8,10 +8,7 @@
     (setq python-shell-interpreter "ipython"
 	  python-shell-interpreter-args "--colors=Linux --profile=default"
 	  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-	  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-	  python-shell-completion-setup-code      "from IPython.core.completerlib import module_completion"
-	  python-shell-completion-string-code	  "';'.join(module_completion('''%s'''))\n"
-	  python-shell-completion-string-code	  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+	  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "))
   :bind (:map python-mode-map
 	      ;; python-eldoc-at-point is not really useful, instead
 	      ;; use it for sending file to python shell
