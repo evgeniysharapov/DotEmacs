12211b68693adfae337059e7893f6dad077be17a
Evgeniy Sharapov <esharapov@infusion.com>
2017-01-13 16:18:22 -0500

    added 4clojure.

--- site-lisp/ffe-clojure.el
+++ site-lisp/ffe-clojure.el
@@ -8,4 +8,17 @@
   (add-hook 'cider-repl-mode-hook #'paredit-mode)
   (add-hook 'cider-repl-mode-hook #'eldoc-mode))
 
+(use-package 4clojure
+  :commands (4clojure-open-question)
+  :after cider
+  :ensure t
+  :defer t
+  :init (progn
+	  (defadvice 4clojure-open-question (around 4clojure-open-question-around)
+	    "Start a cider/nREPL connection if one hasn't already been started when
+opening 4clojure questions"
+	    ad-do-it
+	    (unless cider-current-clojure-buffer
+	      (cider-jack-in)))))
+
 (provide 'ffe-clojure)
