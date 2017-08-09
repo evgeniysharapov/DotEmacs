204b1bf608b540f1f7686f1f491c79b583dccf57
Evgeniy Sharapov <esharapov@infusion.com>
2017-08-09 08:58:05 -0400

    Use regular python interpreter instead of ipython

--- site-lisp/ffe-python.el
+++ site-lisp/ffe-python.el
@@ -1,17 +1,25 @@
 ;;; Python Configuration
-
+;;;
+;;; make sure to install pyreadline on Windows.
+;;;
 (use-package python
   :defer t
   :commands python-mode
   :config
-  (when (executable-find "ipython")
-    (setq python-shell-interpreter "ipython"
-  	  ;python-shell-prompt-input-regexps "In \\[[0-9]+\\]: "
-  	  ;python-shell-prompt-output-regexps "Out\\[[0-9]+\\]: "
-          python-shell-prompt-block-regexp "\\s-*\\.\\.\\."
-  	  python-shell-completion-setup-code  "from IPython.core.completerlib import module_completion"
-  	  python-shell-completion-string-code  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
-     ))
+  (progn
+    ;; (when (executable-find "ipython")
+    ;;   (setq python-shell-interpreter "ipython"
+    ;;                                     ;python-shell-prompt-input-regexps "In \\[[0-9]+\\]: "
+    ;;                                     ;python-shell-prompt-output-regexps "Out\\[[0-9]+\\]: "
+    ;;         ;;python-shell-interpreter-args "--simple-prompt --pprint"
+    ;;         ;;python-shell-prompt-block-regexp "\\s-*\\.\\.\\."
+    ;;         ;;python-shell-completion-setup-code  "from IPython.core.completerlib import module_completion"
+    ;;         ;;python-shell-completion-string-code  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+    ;;         )
+    ;;   ;; set the PAGER to 'less', otherwise IPython keeps failing
+    ;;   (setenv "PAGER" "less"))
+    
+    (add-hook 'python-mode-hook #'eldoc-mode))
   :bind (:map python-mode-map
 	      ;; python-eldoc-at-point is not really useful, instead
 	      ;; use it for sending file to python shell
@@ -32,8 +40,7 @@
   :init (progn
 	  (setq anaconda-mode-installation-directory (concat *data-dir* "anaconda-mode"))
           (add-hook 'python-mode-hook #'anaconda-mode)
-          (add-hook 'python-mode-hook #'anaconda-eldoc-mode)
-	  (add-hook 'python-mode-hook #'eldoc-mode))
+          (add-hook 'python-mode-hook #'anaconda-eldoc-mode))
   :bind (:map anaconda-mode-map
 	      ;; make key bindings more traditional
 	      ("M-," . anaconda-mode-go-back)
