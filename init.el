7fc25a2323bfef9cec1c0fa61984493dd23cc7da
Evgeniy Sharapov <esharapov@infusion.com>
2019-01-14 14:30:44 -0500

    moved ui configuration into init.el

--- init.el
+++ init.el
@@ -53,6 +53,8 @@
 ;;; Utility Packages
 (use-package s :ensure t :defer t)
 (use-package dash :ensure t :defer t)
+;;  Using `diminish'
+(use-package diminish :ensure t)
 
 ;;; Keymap and Keys Organization 
 
@@ -81,7 +83,158 @@
   (define-key ctl-x-map [(control ?z)] c-x-z))
 
 ;;; UI
-(use-package ffe-ui :demand t)
+;; Configuring User experience and UI
+;;;; Fonts
+;;;;; Good typefaces to consider 
+;; (set-frame-font "Cousine-11" t)
+;; (set-frame-font "Iosevka NF-12:light" t)
+;; (set-frame-font "Meslo LG S DZ" t)
+;; (set-frame-font "Knack NF" t)
+;; (set-frame-font "MesloLGSDZ NF" t)
+;; (set-frame-font "MesloLGMDZ NF" t)
+;; (set-frame-font "CamingoCode" t)
+;; (set-frame-font "InputMonoCondensed Light:light" t)
+;; (set-frame-font "InputSerifCondensed Light:light" t)
+;; (set-frame-font "InputSansCondensed Light:light" t)
+;; (set-frame-font "InputMonoNarrow Light:light" t)
+;; (set-frame-font "InputSerifNarrow Light:light" t)
+;; (set-frame-font "InputSansNarrow Light:light" t)
+;; (set-frame-font "InputSerif" t)
+;; (set-frame-font "InputSans" t)
+;; (set-frame-font "Fira Code" t)
+;; (set-frame-font "Fira Mono for Powerline" t)
+;; (set-frame-font "FantasqueSansMono NF-11" t)
+;; (set-frame-font "Liberation Mono-11" t)
+;; (set-frame-font "Consolas-11" t)
+;; (set-frame-font "LiterationMonoPowerline NF" t)
+;; (set-frame-font "InconsolataForPowerline NF" t)
+;;;;; Font Families
+(defconst ffe-font-families '(
+			      "Anonymous Pro"
+			      "Bitstream"
+			      "CamingoCode"
+			      "Code New Roman"
+			      "Consolas"
+			      "Cousine"
+			      "DejaVu"
+			      "Envy Code R"
+			      "Fantasque"
+			      "Fira"
+			      "Hack"
+			      "Inconsolata"
+			      "Input"
+			      "Iosevka"
+			      "Knack"
+			      "Liberation Mono"
+			      "Literation"
+			      "Lucida Console"
+			      "M+"
+			      "Menlo"
+			      "Meslo"
+			      "Monaco"
+			      "Mono"
+			      "Monoid"
+			      "NotCourierSans"
+			      "Pragmata"
+			      "Roboto Mono"
+			      "Source Code"
+			      "mononoki"
+			      ))
+;; This is regexp to filter out fonts that are not in the `ffe-font-families'
+(defconst --filter-fonts (regexp-opt ffe-font-families) "Regular expression that covers possible usability")
+
+;;;;; Selecting A Font for Frame 
+(defun ffe-select-typeface ()
+  "Choose typefaces for the frame"
+  (interactive)
+  (set-frame-font (ido-completing-read+ "Choose font:"
+					(cl-concatenate 'list
+							'("Iosevka Light-12"
+							  "Iosevka-12"
+							  "Hack-11"
+							  "Meslo LG S DZ-11"
+							  "Cousine-12"
+							  "InputMonoCondensed-12"
+							  "PragmataPro-12")
+							(cl-remove-if-not
+							 (lambda (e) (string-match-p --filter-fonts e))
+							 (font-family-list))
+							 ))
+		  t))
+
+(bind-key "f" #'ffe-select-typeface ctl-x-t-map)
+
+;;;; Windows
+(use-package windmove
+  :ensure t
+  :defer t
+  :bind (:map ctl-x-w-map
+              ("<left>" . windmove-left)
+              ("h" . windmove-left)
+              ("<right>" . windmove-right)
+              ("l" . windmove-right)
+              ("<up>" . windmove-up)
+              ("j" . windmove-up)
+              ("<down>" . windmove-down)
+              ("k" . windmove-down)))
+
+(use-package ace-window
+  :ensure t
+  :pin melpa-stable
+  :bind (:map ctl-x-w-map
+              ("w" . ace-window)))
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(bind-key "|" 'toggle-window-split ctl-x-w-map)
+
+;;;; Look and Feel
+(use-package whitespace
+  :bind (:map ctl-x-t-map
+              ("SPC" . whitespace-mode)))
+
+(use-package rainbow-mode
+  :ensure t
+  :bind (:map ctl-x-t-map
+              ("r" . rainbow-mode)))
+
+(use-package crosshairs
+  :bind (:map ctl-x-t-map
+              ("+" . crosshairs-mode)))
+
+;; there's no point in hiding menubar on macos
+(when (not (string= system-type "darwin"))
+  (custom-set-minor-mode 'menu-bar-mode nil))
+(custom-set-minor-mode 'tool-bar-mode nil)
+(custom-set-minor-mode 'scroll-bar-mode nil)
+
+(when (display-graphic-p)
+  (custom-set-minor-mode 'mouse-wheel-mode t)
+  (custom-set-minor-mode 'blink-cursor-mode nil))
+
 
 ;;; Files
 (use-package ffe-files)
