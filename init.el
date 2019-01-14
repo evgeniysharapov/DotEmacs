f66c5190a4e2220c0e67bf7eceaa171d4ac91937
Evgeniy Sharapov <esharapov@infusion.com>
2019-01-14 16:06:54 -0500

    moved spellcheck and help/info functionality to init

--- init.el
+++ init.el
@@ -55,6 +55,7 @@
 (use-package dash :ensure t :defer t)
 ;;  Using `diminish'
 (use-package diminish :ensure t)
+(use-package subr-x)
 
 ;;;; Ido Configuration
 (use-package ido
@@ -593,10 +594,111 @@
 (bind-key "C-x K" #'ffe-kill-current-buffer)
 
 ;;; Spellcheck
-(use-package ffe-spell)
+;; On Windows use pre-compiled hunspell
+;; For Mac OS X install `aspell' using Homebrew
+;;
+;;     brew install aspell --with-all-langs
+;; 
+;; In order for spelling to work correctly it expects DICTIONARY
+;; environment variable to be set
+;; Trick from http://www.emacswiki.org/emacs/InteractiveSpell
+(setenv "DICTIONARY" "en_US")
+
+;; note that for aspell file has to have a first line
+;; personal_ws-1.1 en 0
+;;    https://blog.samat.org/2008/11/02/creating-your-own-personal-aspell-dictionary/
+(defun ffe-personal-dictionary ()
+  "Personal dictionary setup if file doesn't exist then create it"
+  (let ((personal-dictionary-file (concat *data-dir* ".personal.dict")))
+    (unless (file-exists-p personal-dictionary-file)
+      (with-temp-file personal-dictionary-file t))
+    personal-dictionary-file))
+
+(use-package flyspell
+  :commands (flyspell-buffer flyspell-mode flyspell-region)
+  :config (setq flyspell-use-meta-tab nil
+		flyspell-auto-correct-binding (kbd "C-M-;"))
+  :bind (:map flyspell-mode-map
+	      ("C-," . nil)
+	      ("C-." . nil)
+	      ("C-;" . nil)
+	      ("C-c $" . nil)
+	      ("C-$ n" . flyspell-goto-next-error)
+	      ("C-$ b" . flyspell-buffer)
+	      ("C-$ r" . flyspell-region)))
+
+(use-package ispell
+  :defer t
+  :config (setq ispell-personal-dictionary (ffe-personal-dictionary)
+		ispell-silently-savep t
+		ispell-choices-win-default-height 3)
+  :init (progn
+	  ;; Aspell Specific
+	  (when (executable-find "aspell")
+	    (setq ispell-program-name "aspell"
+		  ispell-extra-args '("--sug-mode=ultra")))
+	  ;; Hunspell Specific
+	  (when (executable-find "hunspell")
+	    (setq ispell-program-name "hunspell"))))
 
 ;;; Help
-(use-package ffe-help)
+;; Help/Info configuration
+;;;; Emacs Help
+(use-package help-mode+)
+(use-package help+)
+(use-package help-fns+)
+
+;; How often I need to see emacs FAQ. Use C-h C-f as a keymap for find-* commands
+(defvar help-find-map)
+(define-prefix-command 'help-find-map)
+(bind-key "C-f" 'help-find-map help-map)
+
+;; I don't really need to see view-hello-file either 
+(defvar help-help-map)
+(define-prefix-command 'help-help-map)
+(bind-key "h" 'help-help-map help-map)
+
+
+(use-package help
+  :config (progn
+            (defun ffe-describe-symbol-at-point ()
+              "Describes symbol at a point"
+              (interactive)
+              (when-let (symbol (symbol-at-point))
+                (describe-symbol symbol)))
+
+            (defun ffe-quit-windows-on-help ()
+              "Quits window with *Help* buffer"
+              (interactive)
+              (quit-windows-on "*Help*")))  
+  :bind (:map help-map
+              ("C-b" . describe-personal-keybindings)
+              ("C-k" . describe-key-briefly)
+              ("C-c" . describe-char)
+              ("C-s" . ffe-describe-symbol-at-point)
+              ("C-S-s" . describe-symbol)
+              ;; Too often I need to close Help windows after seeing the doc and I don't really use `help-quit' anyway
+              ([remap help-quit] . ffe-quit-windows-on-help)
+              ;; help-for-help is nice, but I would like C-h still show me prefix bindings
+              ("C-h" . describe-prefix-bindings)
+              
+         ;; finding a variable or a function or a library, i.e. opening them in the editor
+         :map help-find-map
+              ("f" . find-function)
+              ("v" . find-variable)
+              ("l" . find-library)))
+
+(use-package helpful
+  :ensure t
+  :bind (:map help-help-map
+              ("f" . helpful-function)
+              ("v" . helpful-variable)
+              ("c" . helpful-command)
+              ("m" . helpful-macro)
+              ("h" . helpful-at-point)))
+
+
+;;;; Info
 
 ;;; Tweaks
 ;; short response function instead of long one
@@ -771,7 +873,7 @@
 
 ;;; Misc File Formats
 
-;;;;  JSON
+;;;; JSON
 (use-package ffe-json)
 
 ;;;; NGinx Configuration
@@ -833,7 +935,7 @@
   :init (setq flyspell-generic-check-word-predicate
 	      'flyspell-markdown-check-word-predicate))
 
-;;;;  LESS 
+;;;; LESS 
 (use-package less-css-mode
   :ensure t)
 
