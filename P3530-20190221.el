64ef79dd9054ee856305150abf0f5aa5a8c748f9
Evgeniy Sharapov <esharapov@infusion.com>
2019-03-19 10:57:09 -0400

    added separate initialization for Emacs run from WSL and Emacs run in Windows.

--- P3530-20190221.el
+++ P3530-20190221.el
@@ -3,9 +3,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'default-frame-alist '(font . "Iosevka-12"))
+(when (string-equal system-type "gnu/linux")
+  ;; Emacs is running from WSL
+  (add-to-list 'default-frame-alist '(font . "Iosevka-13"))
+  (set-face-attribute 'default (selected-frame) :height 135))
 
+(when (string-equal system-type "windows-nt")
+  ;; Emacs is running from Windows
+  (add-to-list 'default-frame-alist '(font . "Iosevka-12"))
+  (setq w32-pass-lwindow-to-system nil
+        w32-lwindow-modifier 'super    ; Left Windows key
+        w32-pass-apps-to-system nil
+        w32-apps-modifier 'hyper)      ; Menu/App key
+  )
 
+;;; P3530-20190221.el ends here
 
 
 
