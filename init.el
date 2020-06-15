b7ceba122a149a9ed87d74d71ac5555218afd52a
Evgeniy N. Sharapov <esharapov@ainq.com>
2020-06-15 11:47:47 -0400

    prog: javascript is configured with LSP

--- init.el
+++ init.el
@@ -1112,11 +1112,11 @@ Due to a bug http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16759 add it to a c-mo
 
 
 ;;;; Javascript
-;; Javascript Configuration
 ;;
 ;; Node packages expected to be installed globally:
-;; 1. tern
-;; 2. mocha
+;; npm i -g tern
+;; npm install -g javascript-typescript-langserver
+
 (use-package js2-mode
   :defer t
   :diminish (javascript-mode . "JS")
@@ -1131,46 +1131,22 @@ Due to a bug http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16759 add it to a c-mo
     (add-hook 'js2-mode-hook #'electric-pair-mode)
     (add-hook 'js2-mode-hook #'hs-minor-mode)))
 
-(use-package tern
-  :commands (tern-mode)
-  :ensure t
-  :init
-  (add-hook 'js2-mode-hook #'tern-mode))
-
-;; We can't quite manipulate local mode keymap from within a hook, so we do it in EAL form
-(eval-after-load "tern"
-  '(progn
-     ;; clear C-c C-r and C-c C-d from tern-keymap
-     (define-key tern-mode-keymap (kbd  "C-c C-r") nil)
-     (define-key tern-mode-keymap (kbd "C-c C-d") nil)
-     (bind-keys :map tern-mode-keymap
-                ("C-c C-r r" . tern-rename-variable)
-                ("C-c C-d C-d" . tern-get-docs)
-                ("C-c C-d d" . tern-get-type))))
-
-
 (use-package company-tern
-  :after company
   :ensure t
-  :init (progn
-          (defun ffe-js2-mode-company-hook ()
-            (ffe-add-company-backends 'company-tern 'company-semantic))
-          (add-hook 'js2-mode-hook #'ffe-js2-mode-company-hook)))
+  ;; :ensure-system-package
+  ;; ((tern . "npm install -g tern"))
+  :after js2-mode
+  :hook '((js2-mode . company-mode)
+          (js2-mode . tern-mode))
+  :config
+  (add-to-list 'company-backends 'company-tern)
+  ;; use LSP for navigation
+  (define-key tern-mode-keymap (kbd "M-.") nil)
+  (define-key tern-mode-keymap (kbd "M-,") nil)
+  ;; use js2 refactor for  tenames
+  (define-key tern-mode-keymap (kbd "C-c C-r") nil))
 
-(use-package mocha
-  :commands (mocha-test-at-point mocha-test-file mocha-test-project)
-  :ensure t
-  :pin melpa-stable
-  :bind (:map js2-mode-map
-              ("C-c t ." . mocha-test-at-point)
-              ("C-c t f" . mocha-test-file)
-              ("C-c t p" . mocha-test-project)))
-
-(use-package js-comint
-  :ensure t
-  :commands (run-js))
-
-;; customize projectile
+;;;;; Customize Projectile
 (when (fboundp 'projectile-register-project-type)
   (projectile-register-project-type 'npm '("package.json") :test "npm test"  :test-suffix ".spec"))
 
