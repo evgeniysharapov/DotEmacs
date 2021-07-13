198e7929bbe1acd9decc5467f9f0ce4cff535c84
Evgeniy N. Sharapov <esharapov@ainq.com>
2021-07-13 10:12:28 -0400

    Added configuration for replacement laptop

---
+++ DESKTOP-OU44UDR.el
@@ -0,0 +1,50 @@
+;;; package --- summary
+;;; Configuration for Replacemet  Laptop from Ai
+;;; Commentary:
+;;; Code:
+
+(defun set-font-if-available (font-name)
+  "Checks if font FONT-NAME is available and sets it as a default for a frame."
+  (when (find-font (font-spec :name font-name))
+    (add-to-list 'default-frame-alist  (cons 'font font-name))))
+
+(defun try-fonts (font-name-list)
+  "Goes over list FONT-NAME-LIST and tries to set a font name from the list as the default.
+
+ Stops once it either exhausted the list or successfully installed the font."
+  (cl-loop for font-name in font-name-list
+           until (set-font-if-available font-name)))
+
+;;; Emacs is Running from WSL
+(when (string-equal system-type "gnu/linux")
+  ;; Emacs is running from WSL
+
+  ;; for running 'emacs --daemon' we store .emacs.desktop in a different spot
+  (add-hook 'desktop-save-hook
+            (lambda ()
+              (customize-set-variable 'desktop-base-file-name ".emacs.desktop.wsl")))
+
+  (add-to-list 'default-frame-alist '(font . "Iosevka"))
+  (set-face-attribute 'default (selected-frame) :height 140))
+
+;;; Emacs is Running from Windows
+(when (string-equal system-type "windows-nt")
+  ;; Emacs is running from Windows
+  (try-fonts '("Iosevka"
+               "Iosevka SS09"
+               "Consolas"
+               "Cascadia Code PL"))
+  (setq w32-pass-lwindow-to-system nil
+        w32-lwindow-modifier 'super    ; Left Windows key
+        w32-pass-apps-to-system nil
+        w32-apps-modifier 'hyper)      ; Menu/App key
+  ;; Put my tools bin directory in front of the path
+  
+  (setenv "PATH" (concat "C:\\Users\\esharapov\\Tools\\Bin;" (getenv "PATH")))
+
+  (setq ffip-use-rust-fd t)
+)
+;;; P3530-20190221.el ends here
+
+
+
