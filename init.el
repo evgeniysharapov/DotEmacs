58e791db989e61d99a5d8ae0c696a7234b7f0e45
Evgeniy Sharapov <sharape@labcorp.com>
2024-07-22 14:56:03 -0400

    modify loading paredit

--- init.el
+++ init.el
@@ -1193,8 +1193,9 @@ Examples:
 ;;;; General Settings 
 (use-package eldoc
   :diminish eldoc-mode
-  :commands eldoc-mode
-  :init  (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode))
+  :commands global-eldoc-mode
+  :config
+  (global-eldoc-mode))
 
 (use-package hl-line-mode
   :commands hl-line-mode
@@ -1262,33 +1263,35 @@ Examples:
          (lsp-mode . dap-ui-mode)))
 
 ;;;; Lisp
-;; This is a lisp based programming language configuration
-;; mostly Emacs lisp and IELM and such
-(defconst *lisp-mode-hooks* '(emacs-lisp-mode-hook
-			      ielm-mode-hook
-			      lisp-mode-hook
-			      lisp-interaction-mode-hook))
-
+;;;;; General Lisp 
 (use-package paredit
   :ensure t
-  :diminish paredit-mode
-  :commands paredit-mode
-  :config (progn (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
-		 ;; we use M-s for searching stuff
-		 (unbind-key "M-s" paredit-mode-map)
-		 ;; bind splice onto M-k since we shouldn't use it in lisp
-		 ;; mode anyway
-		 (bind-key "M-k" #'paredit-splice-sexp paredit-mode-map)))
+  :commands enable-paredit-mode
+  :hook
+  (emacs-lisp-mode . enable-paredit-mode)
+  (ielm-mode .enable-paredit-more)
+  (lisp-mode . enable-paredit-more)
+  (lisp-interaction-mode . enable-paredit-mode)
+  (eval-expression-minibuffer-setup . enable-paredit-mode)
+  :bind
+  (:map paredit-mode-map
+        ("M-s" . nil)   ;; we use M-s for searching stuff
+        ("M-k" . paredit-splice-sexp) ;; bind splice onto M-k since we shouldn't use it in lisp mode anyway
+        ))
 
+;; (use-package parinfer-rust-mode
+;;   :ensure t
+;;   :custom
+;;   (parinfer-rust-disable-troublesome-modes 't)
+;;   (parinfer-rust-check-before-enable 'defer)
+;;   (parinfer-rust-auto-download nil))
+
+;;;;; Emacs Lisp 
 (use-package elisp-slime-nav
   :ensure t
-  :commands elisp-slime-nav-mode
-  :diminish elisp-slime-nav-mode)
-
-(dolist (mode-hook *lisp-mode-hooks*)
-  (add-hook mode-hook #'paredit-mode)
-  (add-hook mode-hook #'elisp-slime-nav-mode)
-  (add-hook mode-hook #'eldoc-mode))
+  :diminish elisp-slime-nav-mode
+  ;:hook (emacs-lisp-mode ielm-mode lisp-mode lisp-interaction-mode eval-expression-minibuffer-setup)
+  )
 
 ;; (global-set-key [remap eval-expression] 'pp-eval-expression)
 ;; (global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
@@ -1296,18 +1299,21 @@ Examples:
 
 ;; turn off checkdoc for my configuration files
 (defun ffe-disable-elisp-checkdoc-in-configuration-files ()
-            (if (and (eq major-mode 'emacs-lisp-mode)  ; if it is elisp
-		     (or
-		      (string-equal user-init-file (buffer-file-name))	; or init.el file
-		      (string-equal custom-file (buffer-file-name)) ; customization file
-		      (and 		; configuration modules
-		       (string-match (or (file-name-directory (or  (buffer-file-name) "")) "") *lisp-dir*)
-		       (string-match "^ffe-" (or (file-name-nondirectory (or  (buffer-file-name) "")) "")))))
-                (flycheck-disable-checker 'emacs-lisp-checkdoc)))
+  (if (and (eq major-mode 'emacs-lisp-mode)  ; if it is elisp
+		   (or
+		    (string-equal user-init-file (buffer-file-name))	; or init.el file
+		    (string-equal custom-file (buffer-file-name)) ; customization file
+		    (and 		; configuration modules
+		     (string-match (or (file-name-directory (or  (buffer-file-name) "")) "") *lisp-dir*)
+		     (string-match "^ffe-" (or (file-name-nondirectory (or  (buffer-file-name) "")) "")))))
+      (flycheck-disable-checker 'emacs-lisp-checkdoc)))
 
 (add-hook 'emacs-lisp-mode-hook #'ffe-disable-elisp-checkdoc-in-configuration-files)
 
-(defun ffe-ielm ()
+(use-package ielm
+  :defer t
+  :init
+  (defun ffe-ielm ()
   "Starts IELM or switches to existing one in the new window and sets working buffer of IELM to the current buffer."
   (interactive)
   (let ((buf (current-buffer)))
@@ -1318,9 +1324,8 @@ Examples:
         (other-window 1)
         (ielm)))
     (ielm-change-working-buffer buf)))
-
-(bind-key "C-M-:" #'ffe-ielm)
-
+  :bind
+  ("C-M-:" .  ffe-ielm))
 
 ;;;; Clojure
 (use-package cider
@@ -1329,8 +1334,8 @@ Examples:
   :commands (cider-jack-in)
   :config
   (dolist (hook '(cider-mode-hook cider-repl-mode-hook clojure-mode-hook clojurescript-mode-hook))
-      (add-hook hook #'paredit-mode)
-      (add-hook hook #'eldoc-mode)))
+    (add-hook hook #'paredit-mode)
+    (add-hook hook #'eldoc-mode)))
 
 (use-package 4clojure
   :commands (4clojure-open-question)
