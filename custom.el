4dcf6b41ec97c6159b9723fb0c4e523b3e8b0977
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2024-08-13 11:41:29 -0400

    tex config for newline/indenting on RET

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-newline-function 'reindent-then-newline-and-indent)
  '(anaconda-mode-eldoc-as-single-line t)
  '(auto-image-file-mode t)
  '(aw-dispatch-always t)
